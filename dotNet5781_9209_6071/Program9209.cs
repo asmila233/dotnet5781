f5125b681977e9ba8add3392587ec459cf388d7c
Raz <razaviad@g.jct.ac.il>
2020-10-15 22:50:55 +0300

    craete the func Program

---
+++ dotNet5781_9209_6071/Program9209.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotNet5781_9209_6071
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            Welcome9209();
+            Welcome6071();
+            Console.ReadKey();
+        }
+
+        private static void Welcome6071() { }
+
+        private static void Welcome9209()
+        {
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("{0} , welcome to my first console application", name);
+        }
+    }
+}
